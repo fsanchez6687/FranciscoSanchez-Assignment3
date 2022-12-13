2ba8b38ed4f50f80bb7767526e11ff37eb74e744
Francisco Sanchez <sanchezguevara.frank@gmail.com>
2022-12-13 17:06:15 -0500

    Final

---
+++ src/UserService.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class UserService {
+	
+	public User validateUser(String username, String password) throws IOException {
+		UserArrayService userArrayService = new UserArrayService();
+		for (User user : userArrayService.getUserArray()) {
+			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ src/UserValidation.java
@@ -0,0 +1,49 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UserValidation {
+	public User[] users = new User[4]; 
+	private UserService userService = new UserService();
+	
+	UserArrayService userArrayService = new UserArrayService();
+
+	public static void main(String[] args) throws IOException {
+		UserValidation validation = new UserValidation();
+		validation.program();
+		
+	}
+	
+	public void program () throws IOException {
+		
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(System.in);
+			
+			boolean validLogin = false;
+			int loginAttempts = 0;
+			while (!validLogin && loginAttempts < 5) {
+				System.out.println("Enter your email: ");
+				String username = scanner.nextLine();
+				System.out.println("Enter your password: ");
+				String password = scanner.nextLine();
+
+				User validUser = userService.validateUser(username, password);
+				if (validUser != null) {
+					System.out.println("Welcome: " + validUser.getName());
+					validLogin = true;
+				} else {
+					System.out.println("Invalid login, please try again");
+					loginAttempts++;
+					if (loginAttempts == 5) {
+						System.out.println("Too many failed login attempts, you are now locked out.");
+					}
+				}
+			}
+		} finally {
+			if (scanner != null)
+				scanner.close();
+		}
+	
+	}
+}
